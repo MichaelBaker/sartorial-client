6d8b0884f189e2796777ff960c437e82c06aff64
Michael Baker <michaeltbaker@gmail.com>
2013-12-15 21:22:11 +0000

    Supports players chatting

--- src/Main.hs
+++ src/Main.hs
@@ -9,7 +9,9 @@ import Data.Default
 import JavaScript.JQuery           (click, select)
 import Protocol                    (Request (..), Response (..))
 import Control.Concurrent.STM      (atomically)
-import Control.Concurrent.STM.TVar (newTVarIO, writeTVar)
+import Control.Concurrent.STM.TVar (newTVarIO, writeTVar, readTVar, readTVarIO)
+import Data.List                   (isPrefixOf)
+import Safe                        (readMay)
 
 foreign import javascript interruptible
   "jQuery.ajax('/command', {type: 'POST', contentType: 'text/plain; charset=UTF-8', data: $1}).always(function (data) { $c(data) });"
@@ -22,6 +24,10 @@ foreign import javascript safe
 foreign import javascript unsafe
   "jQuery($1).val()"
   getValue :: JSString -> IO JSString
+
+foreign import javascript unsafe
+  "jQuery($1).val('')"
+  clearValue :: JSString -> IO JSString
 
 foreign import javascript unsafe
   "jQuery($1).html($2)"
@@ -37,32 +43,69 @@ foreign import javascript unsafe
 
 main = do
   playerName <- newTVarIO ""
+  messages   <- newTVarIO []
   setup playerName
-  loop
+  loop  playerName messages
+
+loop playerName messages = do
+  updatePlayerList
+  updateChat playerName messages
+  threadDelay 1000000
+  loop playerName messages
+
+updateChat playerName messages = do
+  name <- readTVarIO playerName
+  if null name
+    then return ()
+    else do
+      result <- sendCommand $ ChatUpdatesRequest name
+      case result of
+        ChatUpdatesResponse newMessages -> do
+          messages <- atomically $ do
+            existingMessages <- readTVar messages
+            let finalMessages = take 10 $ existingMessages ++ newMessages
+            writeTVar messages finalMessages
+            return finalMessages
+          setHtml "#chat-messages" $ toJSString $ concatMap (wrapLi) messages
+	_ -> return ()
 
 setup playerName = do
   playerSubmitButton <- select "#add-player-submit"
   click (addPlayer playerName) def playerSubmitButton
+
+  commandSubmitButton <- select "#command-submit"
+  click (submitCommand playerName) def commandSubmitButton
   return ()
 
 addPlayer playerName _ = do
   hideElement "#add-player"
   name <- getValue "#add-player-input"
-  atomically  $ writeTVar playerName name
+  atomically  $ writeTVar playerName (fromJSString name)
   showElement "#command"
   sendCommand $ AddPlayerRequest $ fromJSString name
   return ()
 
-loop = do
-  updatePlayerList
-  threadDelay 1000000
-  loop
+submitCommand playerName _ = do
+  currentCommand <- getValue "#command-input"
+  clearValue "#command-input"
+  processCommand playerName $ fromJSString currentCommand
+  return ()
+
+processCommand playerName commandString | isPrefixOf "say " commandString = say playerName commandString
+                                        | otherwise = return ()
+
+say playerName commandString = do
+  name <- readTVarIO playerName
+  sendCommand $ ChatMessageRequest name $ dropWhile (== ' ') $ drop 3 commandString
+  return ()
 
 sendCommand :: Request -> IO Response
 sendCommand command = do
   result <- sendAjax $ toJSString $ show command
-  let response = fromJSString result
-  return $ read response
+  let responseText = fromJSString result
+  case readMay responseText of
+    Just a  -> return a
+    Nothing -> print ("Error response " ++ responseText ++ " for command " ++ show command) >> return (ErrorResponse responseText)
 
 updatePlayerList = do
   result  <- sendCommand PlayerListRequest
